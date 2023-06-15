c2c95902e2a28e35719241fa365bcd5d528cdace
MasKatx <tky2102137@stu.o-hara.ac.jp>
2023-06-15 10:02:54 +0900

    runner

--- app/src/main/kotlin/tic/kotlin/learning/club/App.kt
+++ app/src/main/kotlin/tic/kotlin/learning/club/App.kt
@@ -6,9 +6,12 @@ import aws.sdk.kotlin.services.dynamodb.model.*
 import aws.sdk.kotlin.services.dynamodb.paginators.*
 import aws.sdk.kotlin.services.dynamodb.waiters.*
 import aws.sdk.kotlin.services.lambda.*
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.RequestHandler
 import java.util.UUID
 import kotlin.reflect.*
 import kotlin.reflect.full.*
+import kotlinx.coroutines.runBlocking
 
 // https://sdk.amazonaws.com/kotlin/api/latest/dynamodb/index.html
 // https://docs.aws.amazon.com/ja_jp/sdk-for-kotlin/latest/developer-guide/kotlin_dynamodb_code_examples.html
@@ -41,16 +44,16 @@ User型のデータをDynamoに入れる際の型変換にはtoMapとtoAttribute
   メソッド名はscanAllとします
 
 3
-  id, nameが1で生成したid, nameのユーザーを検索して表示
+  idが1で生成したidのユーザーを検索して表示
   メソッド名はsearchByKeyとします
-  なお、id, nameは引数で受け取るようにしてください
+  なお、idは引数で受け取るようにしてください
 
 4
   ageが20歳以上のユーザーを検索して表示
   メソッド名はsearchByAgeとします
-  なお、年齢は引数で受け取るようにしてください
+  なお、ageは引数で受け取るようにしてください
 
-以上の4つのメソッドをAppクラス内に作成し、Appクラスのhandlerメソッド内で順番に呼び出してください
+以上の4つのメソッドをAppクラス内に作成し、AppクラスのhandleRequestメソッド内で順番に呼び出してください
 
 */
 
@@ -66,28 +69,28 @@ val TABLE_NAME = "kotlin-learning"
 // インスタンス化
 val utils = Utils()
 
-class App {
-  suspend fun handler() {
+class App : RequestHandler<Map<String, String>, Unit> {
+  override fun handleRequest(event: Map<String, String>?, context: Context?) = runBlocking {
     // ユーザーの情報を設定
     val id = UUID.randomUUID().toString()
-    val name = ""
-    val age = 0
+    val name = "ishida"
+    val age = 20
     val user = User(id, name, age)
 
     println("ユーザーを追加")
-    // TODO: メソッド呼び出し
+    addUser(user)
     println("追加完了\n")
 
     println("全件取得")
-    // TODO: メソッド呼び出し
+    scanAll()
     println("取得完了\n")
 
     println("id検索")
-    // TODO: メソッド呼び出し
+    searchByKey(id)
     println("検索完了\n")
 
     println("年齢で絞り込み")
-    // TODO: メソッド呼び出し
+    searchByAge(20)
     println("取得完了\n")
   }
 
@@ -95,12 +98,12 @@ class App {
     // 型変換
     val itemValues = utils.toAttributeValueMap(utils.toMap(user))
     // テーブル名とitemを指定
-    val req =
-        PutItemRequest {
-          // TODO: 実装
-        }
+    val req = PutItemRequest {
+      tableName = TABLE_NAME
+      item = itemValues
+    }
     // 追加
-    // TODO: 実装
+    DynamoDbClient { region = REGION }.use { ddb -> ddb.putItem(req) }
   }
 
   suspend fun scanAll() {
@@ -108,7 +111,7 @@ class App {
       // テーブル名を指定
       val request = ScanRequest { tableName = TABLE_NAME }
       // 全件取得
-      val response = TODO: 実装
+      val response = ddb.scan(request)
       // 取得結果を出力
       response.items?.forEach { item ->
         item.forEach { mp ->
@@ -120,9 +123,9 @@ class App {
   }
 
   suspend fun searchByKey(id: String) {
-    // idをMapにセット
+    // idとnameをMapにセット
     val keys = mutableMapOf<String, Any>()
-    // TODO: 実装
+    keys["id"] = id
 
     val keyToGet = utils.toAttributeValueMap(keys)
     // テーブル名とキーを設定
@@ -145,10 +148,13 @@ class App {
   suspend fun searchByAge(age: Int) {
     DynamoDbClient { region = REGION }.use { ddb ->
       // テーブル名, 検索条件を指定
-      val query =
-          ScanRequest {
-            // TODO: 実装
-          }
+      val query = ScanRequest {
+        tableName = TABLE_NAME
+        // 条件式を記述. :ageの部分は変数として解釈され, expressionAttributeValuesでセットした値が使用されます
+        filterExpression = "age >= :age"
+        // :ageの値をセット
+        expressionAttributeValues = utils.toAttributeValueMap(mapOf(":age" to age))
+      }
       // 取得
       val response = ddb.scan(query)
       // 取得結果を出力
@@ -162,8 +168,8 @@ class App {
   }
 }
 
-suspend fun main() {
+fun main() {
   // handlerを起動
   val app = App()
-  app.handler()
+  app.handleRequest(null, null)
 }
